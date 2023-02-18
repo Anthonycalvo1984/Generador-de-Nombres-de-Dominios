2154d5a7c836f7249fb43039186469004319c682
Anthonycalvo1984 <anthonycal1984@gmail.com>
2023-02-18 00:13:42 +0000

    agree

--- src/app.js
+++ src/app.js
@@ -8,4 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("button").addEventListener("click", ejecutar);
+};
+
+let pronoun = ["my", "the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let site = [".com", ".us", ".es", ".io"];
+const ejecutar = () => {
+  console.log("Click aqui");
+  let texto;
+  let parrafo;
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < site.length; l++) {
+          console.log(pronoun[i] + adj[j]);
+          parrafo = document.createElement("p");
+          texto = document.createTextNode(
+            pronoun[i] + adj[j] + noun[k] + site[l]
+          );
+          parrafo.appendChild(texto);
+          document.querySelector("#resultados-dominios").appendChild(parrafo);
+        }
+      }
+    }
+  }
 };
